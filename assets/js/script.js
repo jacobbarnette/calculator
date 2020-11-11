80c7da08e37d287c9d38f889a5493396715bcc65
Jacob Barnette <jacobcbarnette@gmail.com>
2020-11-11 18:25:35 -0500

    added basic math functions

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+/* add function */
+function addValues(a, b) {
+    let addedValues = a + b;
+    console.log(addedValues);
+}
+
+/* subtract function */
+function subtractValues(a, b) {
+    let subtractedValues = a - b;
+    console.log(subtractedValues);
+}
+
+/* mulitply function */
+function multiplyValues(a, b) {
+    let mulitpliedValues = a * b;
+    console.log(mulitpliedValues);
+}
+
+/* division function */
+function divideValues(a, b){
+    let dividedValues = a / b;
+    console.log(dividedValues);
+}
+
+
+addValues(5, 10);
+subtractValues(5, 10);
+multiplyValues(5, 10);
+divideValues(5, 10);
