85f90de44c105164715a9f31b166d684b6db6b60
Jacob Barnette <jacobcbarnette@gmail.com>
2020-11-11 19:31:21 -0500

    html for calc

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,8 +22,21 @@ function divideValues(a, b){
     console.log(dividedValues);
 }
 
-
+function operate(operator, a, b){
+    operator = operator.toLowerCase();
+   if(operator === 'addition') {
+       addValues(a, b);
+   } else if(operator === 'subtration') {
+       subtractValues(a, b);
+   } else if(operator === 'multiplication'){
+       multiplyValues(a, b);
+   } else if(operator === 'division'){
+       divideValues(a, b);
+   }
+   
+}
 addValues(5, 10);
 subtractValues(5, 10);
 multiplyValues(5, 10);
 divideValues(5, 10);
+operate('addition', 10, 15);
